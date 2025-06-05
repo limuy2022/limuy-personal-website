3b651457f16493329f86dc91dd8129a6eade12be
limuy <limuyang202011@163.com>
2025-06-05 12:22:09 +0000

    sync

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ import HelloWorld from './components/HelloWorld.vue'
   <RouterView />
 </template>
 
-<style scoped>
+<!-- <style scoped>
 header {
   line-height: 1.5;
   max-height: 100vh;
@@ -82,4 +82,4 @@ nav a:first-of-type {
     margin-top: 1rem;
   }
 }
-</style>
+</style> -->
